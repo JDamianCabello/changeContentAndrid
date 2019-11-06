0820682c22ae753bf4bc2b3ea78e685bf5a09748
Damián Cabello <jdamiancabello@gmail.com>
2019-11-06 09:32:08 +0100

    Realizado.

--- app/src/main/java/es/jdamiancabello/widgetstring/MainActivity.java
+++ app/src/main/java/es/jdamiancabello/widgetstring/MainActivity.java
@@ -3,33 +3,38 @@ package es.jdamiancabello.widgetstring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.ViewStub;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup rgpTipo;
-    private ViewStub include;
+    private LinearLayout contenido;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        include = findViewById(R.id.includeLayout);
+
+        contenido = findViewById(R.id.llyContenido);
         rgpTipo = findViewById(R.id.rgpTipo);
         rgpTipo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                contenido.removeAllViews();
                 switch (id){
                     case R.id.rbEmpresa:
-                        include.setLayoutResource(R.layout.layout_bussiness);
+                        contenido.addView(LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_bussiness,contenido,false));
                         break;
                     case R.id.rbParticular:
-                        include.setLayoutResource(R.layout.layout_particular);
+                        contenido.addView(LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_particular,contenido,false));
                         break;
                 }
-                include.inflate();
+
             }
         });
     }
